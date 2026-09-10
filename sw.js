760987d4161719e13338b4d8d36e2c8b79f4a3ca
souleydiop <44343615+souleydiop@users.noreply.github.com>
2026-09-10 21:10:13 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,12 @@
-const CACHE_NAME = 'genealogie-cache-v1';
+const CACHE_NAME = 'genealogie-cache-v2';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  './js/db.js',
+  './js/model.js',
+  './js/gedcom.js',
+  './js/app.js',
   './icons/icon-192.png',
   './icons/icon-512.png',
   './icons/icon-512-maskable.png'
